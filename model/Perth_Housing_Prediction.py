6f1e345f3bcd9fe7b657a04cf472704752000eaf
S-Vikhasni <vikhasni2000@yahoo.com>
2024-08-20 20:16:46 +0800

    Update Perth_Housing_Prediction.py

--- model/Perth_Housing_Prediction.py
+++ model/Perth_Housing_Prediction.py
@@ -1,1 +1,8 @@
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.model_selection import GridSearchCV, ShuffleSplit, cross_val_score, train_test_split
+from sklearn.linear_model import Lasso, LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error
